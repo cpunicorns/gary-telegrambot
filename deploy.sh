88d183368962dcd5d4f8c5981f22868a8a70b86c
Lisa Vossen <vossen@sipgate.de>
2023-05-12 13:58:41 +0200

    remove env file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 gcloud functions deploy garybot \
 	--region=europe-west3 \
 	--runtime=go118 \
-	--source=https://source.developers.google.com/projects/famous-badge-386309/repos/garybot/fixed-aliases/ \
+	--source=https://source.developers.google.com/projects/famous-badge-386309/repos/garybot \
 	--entry-point=main \
 	--env-vars-file=.env.yaml \
 	--trigger-http
