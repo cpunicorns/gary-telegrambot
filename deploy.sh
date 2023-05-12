f965356227fbd2979db4f3f6f88a7fa6187ae2c0
Lisa Vossen <vossen@sipgate.de>
2023-05-12 13:58:56 +0200

    remove deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcloud functions deploy garybot \
-	--region=europe-west3 \
-	--runtime=go118 \
-	--source=https://source.developers.google.com/projects/famous-badge-386309/repos/garybot \
-	--entry-point=main \
-	--env-vars-file=.env.yaml \
-	--trigger-http
