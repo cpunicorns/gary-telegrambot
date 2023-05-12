0f0eafc0b272a26cf9bd3f0ac813d15a3ebadf9f
Lisa Vossen <vossen@sipgate.de>
2023-05-12 11:14:56 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-gcloud functions deploy gary-telegrambot \
-	--gen2 \
+#!/bin/bash
+
+gcloud functions deploy garybot \
 	--region=europe-west3 \
 	--runtime=go118 \
-	--source=https://source.developers.google.com/p/famous-badge-386309/r/garybot \
+	--source=https://source.developers.google.com/projects/famous-badge-386309/repos/garybot/fixed-aliases/ \
 	--entry-point=main \
 	--env-vars-file=.env.yaml \
+	--trigger-http
